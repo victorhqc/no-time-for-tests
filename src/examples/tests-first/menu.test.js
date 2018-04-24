99ac3ede6260426f29e875df9b15d9af6bcd82ad
Victor Quiroz <victorhqc@gmail.com>
2018-04-24 20:57:54 +0200

    `test-first`

---
+++ src/examples/tests-first/menu.test.js
@@ -0,0 +1,24 @@
+import deepFreeze from 'deep-freeze';
+
+import {
+ FETCH_MENU_REQUESTED,
+ FETCH_MENU_SUCCEEDED,
+ FETCH_MENU_FAILED,
+} from '../../store/constants';
+
+// import reducer from '../../store/reducers/menu';
+
+/*
+Write a Reducer to handle receiving menu items. It must handle the following logic:
+1. Wait for the items
+2. Set the items
+3. Handle an unexpected error
+ */
+
+describe('menu reducer', () => {
+  it('should wait for items');
+
+  it('should set the items');
+
+  it('should handle unexpected errors');
+});
