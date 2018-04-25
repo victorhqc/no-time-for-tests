2fa9be54519edf23f881c191a58e6cf46935bdad
Victor Quiroz <victorhqc@gmail.com>
2018-04-25 12:52:23 +0200

    Thank you gif

--- src/_slides/ThankYou.jsx
+++ src/_slides/ThankYou.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const ThankYou = () => (
   <section>
-    <h3>Thank You!</h3>
+    <img
+      src="https://media.giphy.com/media/a3IWyhkEC0p32/giphy.gif"
+      alt="dog saying thank you."
+    />
+    <br />
     <a href="https://github.com/victorhqc/no-time-for-tests">https://github.com/victorhqc/no-time-for-tests</a>
   </section>
 );
